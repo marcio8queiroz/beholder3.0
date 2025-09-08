cf6bf70a4c142b65831d9e8b9b1e2929a54eca19
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-09-07 23:08:53 -0400

    useState

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function onChangeEmail(event){
+    setEmail(event.target.value);
+  }
+
+   function onChangePassword(event){
+    setPassword(event.target.value);
+  }
+
+  function btnLoginClick(){
+    console.log(email, password);
+  }
+
   return(
    <main>
         <section className="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
@@ -18,7 +35,7 @@ function Login() {
                                         <span className="input-group-text" id="basic-addon1">
                                             <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                                         </span>
-                                        <input type="email" className="form-control" placeholder="example@company.com" id="email" autoFocus required />
+                                        <input type="email" className="form-control" placeholder="example@company.com" id="email" autoFocus required value={email} onChange={onChangeEmail}/>
                                     </div>  
                                 </div>
                                
@@ -30,12 +47,12 @@ function Login() {
                                             <span clas="input-group-text" id="basic-addon2">
                                                 <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path></svg>
                                             </span>
-                                            <input type="password" placeholder="Password" className="form-control" id="password" required />
+                                            <input type="password" placeholder="Password" className="form-control" id="password" required value={password} onChange={onChangePassword} />
                                         </div>  
                                     </div>
                                 </div>
                                 <div className="d-grid">
-                                    <button type="submit" className="btn btn-gray-800">Sign in</button>
+                                    <button type="button" className="btn btn-gray-800" onClick={btnLoginClick}>Sign in</button>
                                 </div>
                             </form>
                         </div>
