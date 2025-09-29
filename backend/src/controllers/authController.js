c9b6ce94e1d6ba6c68fc6d639bcc9c3146830b53
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-09-29 18:23:51 -0400

    json web token

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,21 +1,37 @@
-async function doLogin(req, res, next) {
+  import jwt from "jsonwebtoken";
+
+  async function doLogin(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
     if(email === "marcio8queiroz@hotmail.com" && password === "123456"){
-        return res.json({
-            id: 1,
-            token: "token"
-        });
+
+        const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
+            expiresIn: parseInt(process.env.JWT_EXPIRES)
+        })
+
+        return res.json({ id: 1,token });
     }
     res.sendStatus(401);
 }
 
+const blacklist = {};
+
 async function doLogout(req, res, next) {
-    res.send("Logout");
+    const token = req.headers["authorization"];
+    blacklist[token] = true;
+
+    setTimeout(() => blacklist[token] = undefined, parseInt(process.env.JWT_EXPIRES) * 1000)
+
+    return res.sendStatus(200);
+}
+
+function isBlacklisted(token){
+    return blacklist[token];
 }
 
 export default {
     doLogin,
-    doLogout
+    doLogout,
+    isBlacklisted
 }
